af063b3b9c5dbd058316ae53de86205017c91718
Oleg Bogachev <bolegcarrick@gmail.com>
2025-05-10 20:36:13 +0300

    6th step half done

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import playEvenGame from "../src/even-game.js";
+import playEvenGame from "../src/games/even-game.js";
 
 playEvenGame();
--- src/even-game.js
+++ src/even-game.js
@@ -1,41 +1,41 @@
-import readlineSync from "readline-sync";
-import greetUser from "./cli.js";
+// import readlineSync from "readline-sync";
+// import greetUser from "./cli.js";
 
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return true;
-  }
-  return false;
-};
+// const isEven = (number) => {
+//   if (number % 2 === 0) {
+//     return true;
+//   }
+//   return false;
+// };
 
-const playEvenGame = () => {
-  console.log("Welcome to the Brain Games!");
-  const userName = greetUser();
+// const playEvenGame = () => {
+//   console.log("Welcome to the Brain Games!");
+//   const userName = greetUser();
 
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+//   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  let correctAnswers = 0;
+//   let correctAnswers = 0;
 
-  while (correctAnswers < 3) {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+//   while (correctAnswers < 3) {
+//     const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-    const correctAnswer = isEven(randomNumber) ? "yes" : "no";
-    console.log(`Question: ${randomNumber}`);
+//     const correctAnswer = isEven(randomNumber) ? "yes" : "no";
+//     console.log(`Question: ${randomNumber}`);
 
-    const userAnswer = readlineSync.question("Your answer: ");
+//     const userAnswer = readlineSync.question("Your answer: ");
 
-    if (userAnswer === correctAnswer) {
-      console.log("Correct!");
-      correctAnswers += 1;
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
-      console.log(`Let's try again, ${userName}`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
+//     if (userAnswer === correctAnswer) {
+//       console.log("Correct!");
+//       correctAnswers += 1;
+//     } else {
+//       console.log(
+//         `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+//       );
+//       console.log(`Let's try again, ${userName}`);
+//       return;
+//     }
+//   }
+//   console.log(`Congratulations, ${userName}!`);
+// };
 
-export default playEvenGame;
+// export default playEvenGame;
