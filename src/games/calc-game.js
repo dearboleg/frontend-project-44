af1a8488c06e56be3f4f587b7402af8c0e3dde16
Oleg Bogachev <bolegcarrick@gmail.com>
2025-05-12 20:45:08 +0300

    7th step done

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -21,30 +21,6 @@ const calcGame = () => {
 
   let correctAnswers = 0;
 
-  //   while (correctAnswers < 3) {
-  //     const randomOperator = () => {
-  //       const operators = ["+", "-", "*"];
-  //       return operators[Math.floor(Math.random() * operators.length)];
-  //     };
-  //     const randomNumber = Math.floor(Math.random() * 100) + 1;
-  //     const num1 = randomNumber();
-  //     const num2 = randomNumber();
-  //     const operator = randomOperator();
-
-  //     const expression = `${num1} ${operator} ${num2}`;
-  //     console.log(`Question: ${expression}`);
-
-  //     const userAnswer = readlineSync.question("Your answer: ");
-  //     const correctAnswer = calculateExpression(num1, num2, operator).toString();
-
-  //     const continueGame = generalLogic(userAnswer, correctAnswer, userName);
-
-  //     if (!continueGame) {
-  //       return;
-  //     }
-
-  //     correctAnswers += 1;
-  //   }
   while (correctAnswers < 3) {
     const randomOperator = () => {
       const operators = ["+", "-", "*"];
--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 import readlineSync from "readline-sync";
 import greetUser from "./cli.js";
 
-// export const greet = () => {
-//   console.log("Welcome to the Brain Games!");
-// };
-
-// export const generalLogic = () => {
-//   if (userAnswer === correctAnswer) {
-//     console.log("Correct!");
-//     correctAnswers += 1;
-//   } else {
-//     console.log(
-//       `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-//     );
-//     console.log(`Let's try again, ${userName}`);
-//     return;
-//   }
-// };
-
 export const generalLogic = (userAnswer, correctAnswer, userName) => {
   if (userAnswer === correctAnswer) {
     console.log("Correct!");
-    return true; // Продолжить игру
+    return true;
   } else {
     console.log(
       `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
     );
     console.log(`Let's try again, ${userName}!`);
-    return false; // Закончить игру
+    return false;
   }
 };
