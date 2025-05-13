8b098ecd56de9c445a21919f2bafa81d492bdab8
Oleg Bogachev <bolegcarrick@gmail.com>
2025-05-13 19:20:01 +0300

    added // eslint-disable-next-line to every Math.random()

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,40 +1,42 @@
-import readlineSync from 'readline-sync'
-import greetUser from '../cli.js'
-import { generalLogic } from '../index.js'
+import readlineSync from "readline-sync";
+import greetUser from "../cli.js";
+import { generalLogic } from "../index.js";
 
 const findGCD = (a, b) => {
   while (b > 0) {
-    const temp = b
-    b = a % b
-    a = temp
+    const temp = b;
+    b = a % b;
+    a = temp;
   }
-  return a
-}
+  return a;
+};
 
 const playGCDGame = () => {
-  const userName = greetUser()
-  console.log('Find the greatest common divisor of given numbers.')
+  const userName = greetUser();
+  console.log("Find the greatest common divisor of given numbers.");
 
-  let correctAnswers = 0
+  let correctAnswers = 0;
 
   while (correctAnswers < 3) {
-    const num1 = Math.floor(Math.random() * 100) + 1
-    const num2 = Math.floor(Math.random() * 100) + 1
+    // eslint-disable-next-line
+    const num1 = Math.floor(Math.random() * 100) + 1;
+    // eslint-disable-next-line
+    const num2 = Math.floor(Math.random() * 100) + 1;
 
-    console.log(`Question: ${num1} ${num2}`)
+    console.log(`Question: ${num1} ${num2}`);
 
-    const userAnswer = readlineSync.question('Your answer: ')
-    const correctAnswer = findGCD(num1, num2).toString()
+    const userAnswer = readlineSync.question("Your answer: ");
+    const correctAnswer = findGCD(num1, num2).toString();
 
-    const continueGame = generalLogic(userAnswer, correctAnswer, userName)
+    const continueGame = generalLogic(userAnswer, correctAnswer, userName);
 
     if (!continueGame) {
-      return
+      return;
     }
 
-    correctAnswers += 1
+    correctAnswers += 1;
   }
-  console.log(`Congratulations, ${userName}!`)
-}
+  console.log(`Congratulations, ${userName}!`);
+};
 
-export default playGCDGame
+export default playGCDGame;
--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,41 +1,42 @@
-import readlineSync from 'readline-sync'
-import greetUser from '../cli.js'
-import { generalLogic } from '../index.js'
+import readlineSync from "readline-sync";
+import greetUser from "../cli.js";
+import { generalLogic } from "../index.js";
 
 const isPrime = (num) => {
-  if (num <= 1) return false
-  if (num <= 3) return true
+  if (num <= 1) return false;
+  if (num <= 3) return true;
 
-  if (num % 2 === 0 || num % 3 === 0) return false
+  if (num % 2 === 0 || num % 3 === 0) return false;
 
   for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false
+    if (num % i === 0 || num % (i + 2) === 0) return false;
   }
-  return true
-}
+  return true;
+};
 
 const playPrimeGame = () => {
-  const userName = greetUser()
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
+  const userName = greetUser();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-  let correctAnswers = 0
+  let correctAnswers = 0;
 
   while (correctAnswers < 3) {
-    const randomNumber = Math.floor(Math.random() * 100) + 1
-    const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no'
-    console.log(`Question: ${randomNumber}`)
+    // eslint-disable-next-line
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = isPrime(randomNumber) ? "yes" : "no";
+    console.log(`Question: ${randomNumber}`);
 
-    const userAnswer = readlineSync.question('Your answer: ')
+    const userAnswer = readlineSync.question("Your answer: ");
 
-    const continueGame = generalLogic(userAnswer, correctAnswer, userName)
+    const continueGame = generalLogic(userAnswer, correctAnswer, userName);
 
     if (!continueGame) {
-      return
+      return;
     }
 
-    correctAnswers += 1
+    correctAnswers += 1;
   }
-  console.log(`Congratulations, ${userName}!`)
-}
+  console.log(`Congratulations, ${userName}!`);
+};
 
-export default playPrimeGame
+export default playPrimeGame;
