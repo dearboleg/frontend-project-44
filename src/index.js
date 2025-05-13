ea6c5762b78545141a7e78f928632e7aa246c6e8
Oleg Bogachev <bolegcarrick@gmail.com>
2025-05-13 18:24:00 +0300

    9th step done, eslint checked

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import readlineSync from "readline-sync";
-import greetUser from "./cli.js";
-
 export const generalLogic = (userAnswer, correctAnswer, userName) => {
   if (userAnswer === correctAnswer) {
-    console.log("Correct!");
-    return true;
-  } else {
-    console.log(
-      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-    );
-    console.log(`Let's try again, ${userName}!`);
-    return false;
+    console.log('Correct!')
+    return true
   }
-};
+  else {
+    console.log(
+      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+    )
+    console.log(`Let's try again, ${userName}!`)
+    return false
+  }
+}
