68478ad41789d90e28624baafd41b25207b833f3
Oleg Bogachev <bolegcarrick@gmail.com>
2025-05-16 17:33:16 +0300

    Добавлены функции generateRandomNumber() и generateRandomNumberSmallStep()

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-// import readlineSync from "readline-sync";
-
-// export const greetUser = () => {
-//   console.log("Welcome to the Brain Games!");
-//   const userName = readlineSync.question("May I have your name? ");
-//   console.log(`Hello, ${userName}!`);
-//   return userName;
-// };
-
-// // export default greetUser
-
-// export const generalLogic = (userAnswer, correctAnswer, userName) => {
-//   if (userAnswer === correctAnswer) {
-//     console.log("Correct!");
-//     return true;
-//   } else {
-//     console.log(
-//       `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-//     );
-//     console.log(`Let's try again, ${userName}!`);
-//     return false;
-//   }
-// };
 import readlineSync from 'readline-sync'
 
 export const greetUser = () => {
@@ -68,4 +45,14 @@ export const runGame = (gameDescription, getGameData) => {
   }
 
   console.log(`Congratulations, ${userName}!`)
+}
+
+export const generateRandomNumber = () => {
+  // eslint-disable-next-line
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+export const generateRandomNumberSmallStep = () => {
+  // eslint-disable-next-line
+  return Math.floor(Math.random() * 10) + 1;
 }
